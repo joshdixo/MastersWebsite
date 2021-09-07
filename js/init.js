0327759d51ee3f51ccecb4f4a54e3d38a2b3f6ff
joshdixo <joshdixon1995@hotmail.co.uk>
2021-09-07 18:40:52 +0100

    Google Maps API integration

--- js/init.js
+++ js/init.js
@@ -5,3 +5,15 @@
 
   });
 })(jQuery);
+
+
+// Google Maps API
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 33.502279623093415, lng: -82.02267051777315 },
+    zoom: 16,
+  });
+}
